9189ae011a665173309a89c8f20cd70d3d03c3ab
lidqqq <lidqqq.dev@gmail.com>
2022-02-05 16:11:35 +0900

    Revert "port 80"
    This reverts commit c6f72c1e82c9b4f58646f3a0265d9b2694531d42.

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,8 @@ import { app } from './app';
 import './handlers';
 
 (async () => {
-  const port = process.env.PORT || 80;
   // アプリを起動します
-  await app.start(port);
+  await app.start(process.env.PORT || 3000);
 
-  console.log(`⚡️ Bolt app is running at port:${port}!`);
+  console.log('⚡️ Bolt app is running!');
 })();
