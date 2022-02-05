c3a7d254d87799f20f844405bf7dfe5691f840d2
lidqqq <lidqqq.dev@gmail.com>
2022-02-05 13:51:45 +0900

    /api

--- api/app.ts
+++ api/app.ts
@@ -4,7 +4,8 @@ import 'dotenv/config';
 // ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: ['/api/slack/events']
 });
 
 export { app };
