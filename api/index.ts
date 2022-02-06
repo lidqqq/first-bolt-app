4e57172ee419a0f42d41148035d859ebda78aad3
lidqqq <lidqqq.dev@gmail.com>
2022-02-06 15:25:03 +0900

    test

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,1 @@
-import { app } from './app';
-import './handlers';
-
-(async () => {
-  // アプリを起動します
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+import '../src/handlers/index';
